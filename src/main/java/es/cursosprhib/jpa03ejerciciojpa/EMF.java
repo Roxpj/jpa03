308c89fdc61b0a1573f19ce8a54df098316afe93
Roxpj <roxanapj.92@gmail.com>
2025-01-21 13:13:59 +0100

    first commit

---
+++ src/main/java/es/cursosprhib/jpa03ejerciciojpa/EMF.java
@@ -0,0 +1,20 @@
+package es.cursosprhib.jpa03ejerciciojpa;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class EMF {
+
+	private static EntityManagerFactory emf;
+	
+	private EMF() {}
+	
+	public static EntityManagerFactory getInstance() {
+	
+		if(emf == null) {
+			emf = Persistence.createEntityManagerFactory("cursojpa");
+		}
+		return emf;
+	}
+	
+}
